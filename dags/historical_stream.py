09a28c8924f574636502f678b1be174c69a3532c
dkon911 <congntdgcd210309@fpt.edu.vn>
2024-11-18 08:30:26 +0700

    Add BashOperator to load data from Cassandra to Postgres in historical_weather_stream DAG

--- dags/historical_stream.py
+++ dags/historical_stream.py
@@ -1,33 +1,42 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 from scripts.historical_weather import historical_weather
 
 
-def fetch_historical_weather():
-    get_date = datetime.now() - timedelta(days=1)
-
-    yesterday = get_date.strftime('%Y-%m-%d')
-
-    historical_weather(start_date=yesterday, end_date=yesterday)
-    # historical_weather(start_date='2024-10-24', end_date='2024-10-25')
-
-
 default_args = {
     'owner': 'airscholar',
-    'start_date': datetime(2024, 8, 1, 20, 00),
+    'start_date': datetime(2024, 8, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
     'timezone': 'Asia/Ho_Chi_Minh'
 }
 
+
+def fetch_historical_weather():
+    get_date = datetime.now() - timedelta(days=1)
+    yesterday = get_date.strftime('%Y-%m-%d')
+    historical_weather(start_date=yesterday, end_date=yesterday)
+
+
+load_cassandra_command = 'spark-submit --master spark://spark-master:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 load_to_DW.py'
+
+# Define the DAG
 with DAG('historical_weather_stream',
-        default_args=default_args,
+         default_args=default_args,
          schedule_interval='15 0 * * *',
-        catchup=False) as dag:
+         catchup=False) as dag:
 
     weather_task = PythonOperator(
         task_id='historical_stream',
         python_callable=fetch_historical_weather
     )
 
-# weather_task >> clean_data>> transform_data >> store_data
+    load_cassandra_task = BashOperator(
+        task_id='load_cassandra_to_postgres',
+        bash_command=load_cassandra_command
+    )
+
+    weather_task >> load_cassandra_task
